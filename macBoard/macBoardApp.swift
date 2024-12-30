6ecb20cd4809082433367320a26e0cb159d380c6
aadishiv <aadishiv@outlook.com>
2024-12-29 23:52:14 -0500

    init

---
+++ macBoard/macBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  macBoardApp.swift
+//  macBoard
+//
+//  Created by Aadi Shiv Malhotra on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct macBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
